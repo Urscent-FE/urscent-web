7d684c08239736bf4a7cf6d35b41cace2802bc45
iamidlek <iamidlek@naver.com>
2022-12-08 00:54:33 +0900

    Feat : 008-라디오, 체크박스 컴포넌트 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,15 +2,23 @@ import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { HomeFooter } from '@/components/homefooter/HomeFooter';
 import { HomeHeader } from '@/components/homeheader/HomeHeader';
-import { Pagenation } from '@/components/pagenation/Pagenation';
+import { RadioLabelBox } from '@/components/radiolabelbox/RadioLabelBox';
 
 export const HomePage = () => {
   const [userLogin, setUserLogin] = useState(false);
-  const [pagenation, setPagenation] = useState(1);
+  const [genderRadio, setGenderRadio] = useState({});
+
   return (
     <div className='init-wrapper'>
       <HomeHeader userLogin={userLogin} />
       <div className='init-body'>
+        <RadioLabelBox
+          labels={['남자', '여자', '남여공용']}
+          group='gender'
+          keyValue={genderRadio}
+          setValue={setGenderRadio}
+          unique
+        />
         <main>
           <Pagenation
             maxPage={49}
