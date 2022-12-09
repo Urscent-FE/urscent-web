87cde72dd822438fdc45e497f24e6d97ce4ff5f6
iamidlek <iamidlek@naver.com>
2022-12-09 23:30:18 +0900

    Feat : 008-range 컴포넌트 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { HomeFooter } from '@/components/homefooter/HomeFooter';
 import { HomeHeader } from '@/components/homeheader/HomeHeader';
 import { RadioLabelBox } from '@/components/radiolabelbox/RadioLabelBox';
+import { RangeInput } from '@/components/rangeInput/RangeInput';
 
 export const HomePage = () => {
   const [userLogin, setUserLogin] = useState(false);
   const [genderRadio, setGenderRadio] = useState({});
+  const [range, setRange] = useState(0);
+  const [range2, setRange2] = useState(0);
+
+  useEffect(() => {
+    console.log(range, range2);
+  }, [range, range2]);
+
+  useEffect(() => {
+    console.log(genderRadio);
+  }, [genderRadio]);
 
   return (
     <div className='init-wrapper'>
@@ -18,6 +29,16 @@ export const HomePage = () => {
           keyValue={genderRadio}
           setValue={setGenderRadio}
           unique
+        />
+        <RangeInput
+          labels={['very short', 'short', 'moderate', 'long', 'very long']}
+          keyValue={range}
+          setValue={setRange}
+        />
+        <RangeInput
+          labels={['은은한', '보통의', '진한', '매우 진한']}
+          keyValue={range2}
+          setValue={setRange2}
         />
         <main>
           <Pagenation
